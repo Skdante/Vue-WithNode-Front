9f651c04c87656e8518605c7eb050eb8d9f84d62
Skdante <32551452+Skdante@users.noreply.github.com>
2020-11-17 09:08:44 -0600

    Carga incial

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    token: null
+  },
+  mutations: {
+    setToken(state, payload) {
+      state.token = payload
+    }
+  },
+  actions: {
+    login({commit}) {
+      
+    }
+  },
+  modules: {
+  }
+})
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Login</h1>
+    <form @submit.prevent="login(usuario)">
+      <input type="email" placeholder="email" v-model="usuario.email">
+      <input type="password" placeholder="password" v-model="usuario.password">
+      <button type="submit">Acceder</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+
+export default {
+  data() {
+    return {
+      usuario: {
+        email: "",
+        password: ""
+      }
+    }
+  },
+  methods: {
+    ...mapActions(['login'])
+  }
+}
+</script>
